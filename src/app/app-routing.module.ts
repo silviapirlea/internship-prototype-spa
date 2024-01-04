0e9cc737db175404fe6ccf05b2bca8372dc44483
silviapirlea <silvia.pirlea@co-era.com>
2024-01-04 12:47:15 +0200

    create the landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./landing-page/landing-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
